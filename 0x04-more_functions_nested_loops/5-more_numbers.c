777edd036307f024fe791411bfb7c4d30e637bfc
RSandagi <wasonga.rina@gmail.com>
2022-03-17 03:21:34 -0700

    .

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,24 +2,26 @@
 
 /**
 * more_numbers - print 0-15  10xand end with new line.
-*
 * Return: 0-15 10x and new line after
 */
 void more_numbers(void)
 {
 
-int i, j;
+  int c, i;
 
-for (i = 0; i <= 10; i++)
-{
+  c = 0;
+  while (c < 10)
+    {
+      i = 0;
+      while (i < 15)
+	{
+	  if (i > 9)
+	    _putchar(i / 10 + '0');
 
-for (j = 0; j <= 14; j++)
-
-{
-if (j >= 10)
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
-}
-putchar('\n');
-}
+	  _putchar(i % 10 + '0');
+	  i++;
+	}
+      c++;
+      _putchar('\n');
+    }
 }
