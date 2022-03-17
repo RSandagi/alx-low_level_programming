82961dc3d8515474f68fa03061adabf5f954d313
RSandagi <wasonga.rina@gmail.com>
2022-03-17 03:31:30 -0700

    .

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,16 +9,23 @@
 
 void print_diagonal(int n)
 {
-int i;
-int j;
+int c, i;
 
-for (i = 0; i < n; i++)
+c = 0;
+
+while (n > 0)
 {
-for (j = 0; j < i; j++)
+i = c;
+while (i > 0)
 {
 _putchar(' ');
+i--;
 }
 _putchar('\\');
 _putchar('\n');
+c++;
+n--;
 }
+if (c < 1)
+_putchar('\n');
 }
